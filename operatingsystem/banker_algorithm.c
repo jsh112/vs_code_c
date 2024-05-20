300a9068e4ef70292c21c339d804ccb462bf32bc
jsh112 <jshkr1120@gmail.com>
2024-05-20 14:28:39 +0900

    banker algorithm

---
+++ operatingsystem/banker_algorithm.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#define p 4
+#define r 3
+
+int main()
+{
+
+    int available[r] = {11,5,4};
+    int work[r] = {0};
+    int max[p][r] = {{7,5,3}, {3, 2, 2}, {9, 0, 4}, {2, 2, 2}};
+    int allocate[p][r] = {{0, 1, 0}, {2, 0, 0}, {4,0,1}, {2, 1, 1}};
+    int need[p][r] = {0};
+    int finished[p] = {0};
+
+    for (int j = 0; j < r; j++){
+        for (int i = 0; i < p; i++){
+            available[j] = available[j] - allocate[i][j];
+            need[i][j] = max[i][j] - allocate[i][j];
+        }
+    }
+    for (int i = 0; i < 3; i++)work[i] = available[i];
+
+    for (int i = 0; i < p; i++){
+        int cnt = 0;
+        for (int j = 0; j < r; j++){
+            if (finished[i] == 0){
+                if (need[i][j] <= work[j])cnt++;
+            }
+        }
+        if (cnt == 3){
+            finished[i] = 1;
+            for (int j = 0; j < r; j++)work[j] = work[j] + allocate[i][j]; 
+            printf("Process %d\n", i + 1);
+            i = -1;
+        }
+    }
+    return 0;
+}
