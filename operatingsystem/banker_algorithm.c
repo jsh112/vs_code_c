0073347cba8dae533f4ef3ed148ba5b65537c185
jsh112 <jshkr1120@gmail.com>
2024-05-27 13:33:09 +0900

    modified:   operatingsystem/banker_algorithm.c

--- operatingsystem/banker_algorithm.c
+++ operatingsystem/banker_algorithm.c
@@ -12,33 +12,23 @@ int main()
     int need[p][r] = {0};
     int finished[p] = {0};
 
-    for (int j = 0; j < r; j++)
-    {
-        for (int i = 0; i < p; i++)
-        {
+    for (int j = 0; j < r; j++){
+        for (int i = 0; i < p; i++){
             available[j] = available[j] - allocate[i][j];
             need[i][j] = max[i][j] - allocate[i][j];
         }
     }
-    for (int i = 0; i < r; i++)
-        work[i] = available[i];
-
-    for (int i = 0; i < p; i++)
-    {
+    for (int i = 0; i < r; i++) work[i] = available[i];
+    for (int i = 0; i < p; i++){
         int cnt = 0;
-        for (int j = 0; j < r; j++)
-        {
-            if (finished[i] == 0)
-            {
-                if (need[i][j] <= work[j])
-                    cnt++;
+        for (int j = 0; j < r; j++){
+            if (finished[i] == 0){
+                if (need[i][j] <= work[j]) cnt++;
             }
         }
-        if (cnt == r)
-        {
+        if (cnt == r){
             finished[i] = 1;
-            for (int j = 0; j < r; j++)
-                work[j] = work[j] + allocate[i][j];
+            for (int j = 0; j < r; j++) work[j] = work[j] + allocate[i][j];
             printf("Process %d\n", i);
             i = -1;
         }
