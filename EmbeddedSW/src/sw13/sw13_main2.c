f370ed392ddbf8c7a1156e195414cf37c97c86ee
jsh112 <jshkr1120@gmail.com>
2024-06-04 11:51:18 +0900

    Update complete code sw13_main2.c

--- EmbeddedSW/src/sw13/sw13_main2.c
+++ EmbeddedSW/src/sw13/sw13_main2.c
@@ -2,88 +2,88 @@
 #include "led_switch.h"
 #include "Timer_Interrupt.h"
 
-int flag1 = 0, flag_cnt = 0;
-int cnt1 = 0, cnt2 = 0, sum = 0;
+unsigned int counter_flag = 0;
+unsigned int cnt1 = 0, cnt2 = 0, sum = 0;
 
-int main(){
+int main()
+{
     hwInit();
 
-    gpio_t led[4] = {ARD_D2, ARD_D3, ARD_D4, ARD_D5};
+    // set gpio led pins and switches
+    set_gpio(&ARD_D0, output);
+    set_gpio(&ARD_D1, output);
+    set_gpio(&ARD_D2, output);
+    set_gpio(&ARD_D3, output);
 
-    set_gpio(&ARD_D0,input); // PC7
-    set_gpio(&ARD_D1,input); // PC6
+    set_gpio(&ARD_D4, input);
+    set_gpio(&ARD_D5, input);
 
-    // LED 1,2,3,4
-    set_gpio(&ARD_D2,output);
-    set_gpio(&ARD_D3,output);
-    set_gpio(&ARD_D4,output);
-    set_gpio(&ARD_D5,output);
-
-    /*
     // RCC_APB2ENR -> SYSCFG Clock Enable
-    *((V_UINT32*)(APB2ENR)) |= (1 << 14);
+    *((V_UINT32 *)(APB2ENR)) |= (1 << 14);
 
-    // Link EXTI 6,7 Interrupt Line
-    *((V_UINT32*)(SYSCFG + EXTICR2)) &= (~(0xF << 4 * 2) | ~(0xF << 4 * 3));
-    *((V_UINT32*)(SYSCFG + EXTICR2)) |= ((0x2 << 4 * 2) | (0x2 << 4 * 3));
+    // Link PG7, PA8 to EXTI
+    *((V_UINT32 *)(SYSCFG + EXTICR2)) &= ~(0xF << 4 * 3);
+    *((V_UINT32 *)(SYSCFG + EXTICR2)) |= (0x06 << 4 * 3);
 
-    *((V_UINT32*)(EXTI + IMR)) |= (1 << 6) | (1 << 7);
+    *((V_UINT32 *)(SYSCFG + EXTICR3)) &= ~(0xF << 0);
 
-    // Set Falling Edge EXTI 6, 7
-    *((V_UINT32*)(EXTI + FTSR)) |= (1 << 6) | (1 << 7);
+    // Enable EXTI7,8 Interrupt line
+    *((V_UINT32 *)(EXTI + IMR)) |= (1 << 7) | (1 << 8);
 
-    // Enable RCC APB2ENR, TIM9EN enable
-    *((V_UINT32*)(APB2ENR)) |= 0x0001;
+    // Set Falling Edge EXTI7,8
+    *((V_UINT32 *)(EXTI + FTSR)) |= (1 << 7) | (1 << 8);
 
-    *((V_UINT32*)(TIM1 + PSC)) &= 0; // PSC Reset
-    *((V_UINT32*)(TIM1 + ARR)) &= 0; // ARR Reset
+    *((V_UINT32 *)(0xE000E100 + (23 / 32) * 4)) = (1 << (23 % 32)); // EXTI9_5_IRQn = 23, NVIC ISER
 
-    *((V_UINT32*)(TIM1 + PSC)) |= 21600 - 1; 			// TIM1 Prescaler
-    *((V_UINT32*)(TIM1 + ARR)) |= 10000 * 1 - 1;  // TIM1 ARR
-    *((V_UINT32*)(TIM1 + DIER)) |= 0x01;  // update interrupt enabled
-    *((V_UINT32*)(TIM1 + CR1)) |= 0x01;   // TIM1 CR1 CEN enable
-    *((V_UINT32*)(TIM1 + SR)) &= 0x01;    // TIM1 SR UIF clear
+    *((V_UINT32*)(0xE000E100 + (25 / 32) * 4)) = (1 << (25 % 32)); // 25, NVIC ISER, TIM1
 
-    *((V_UINT32*)(0xE000E100 + (25 / 32) * 4)) = (1 << (25 % 32)); // 25, NVIC ISER
-
-    *((V_UINT32*)(0xE000E100 + (23 / 32) * 4)) = (1 << (23 % 32)); // EXTI9_5_IRQn = 23, NVIC ISER*/
-    while(1){
-        if(!flag_cnt){
-            for(int i=0;i<4;i++){Signal(&led[3 - i], !((cnt1 >> i) & 0x1));}
-            delay(500);
-            for(int i=0;i<4;i++){Signal(&led[i],1);}
-            delay(500);
-            cnt1++;
-            if(cnt1 == 16) cnt1 = 0;
+    gpio_t led[4] = {ARD_D0, ARD_D1, ARD_D2, ARD_D3};
+    while (1){
+        if (!counter_flag){
+        	cnt2 = 0;
+            for (int j = 0; j < 4; j++){
+                Signal(&led[j], !((sum >> j) & 0x01));
+            }
+            delay(400);
+            for (int j = 0; j < 4; j++){
+                Signal(&led[j], 1);
+            }
+            delay(400);
+            sum++;
+            if (sum == 16) sum = 0;
+            cnt1 = sum;
         }
         else{
-            for(int i=0;i<4;i++){Signal(&led[3 - i], !((cnt2 >> i) & 0x1));}
-            cnt2++;
-            if(cnt2 == 16) cnt2 = 0;
+            for (int j = 0; j < 4; j++){
+                Signal(&led[j], !((cnt2 >> j) & 0x01));
+            }
         }
     }
 }
 
-void EXTI9_5_IRQHandler(){
-	gpio_t led[4] = {ARD_D2, ARD_D3, ARD_D4, ARD_D5};
-    // Check Interrupt 7 is triggered
-    if(*((V_UINT32*)(EXTI + PR)) & (1 << 7)){
-        *((V_UINT32*)(EXTI + PR)) |= (1 << 7);
-        if(flag1 == 0){
-            // initial push switch 1
-            for(int i=0;i<4;i++){Signal(&led[i],1);}
-            flag1 = 1;
+void EXTI9_5_IRQHandler()
+{
+    gpio_t led[4] = {ARD_D0, ARD_D1, ARD_D2, ARD_D3};
+    if ((*((V_UINT32 *)(EXTI + PR)) & (1 << 7)) && !counter_flag)
+    {
+        counter_flag = 1;
+        for(int i=0;i<4;i++){
+            Signal(&led[i], 1);
         }
-        else{
-            sum = cnt1 + cnt2;
-            if(sum >= 16) sum = sum % 16;
-            flag_cnt = 0;
-            cnt1 = sum;
-            flag1 = 0;
-        }
+        *((V_UINT32 *)(EXTI + PR)) |= (1 << 7);
+        delay(100);
     }
-    else if(*((V_UINT32*)(EXTI + PR)) & (1 << 6)){
-        *((V_UINT32*)(EXTI + PR)) |= (1 << 6);
+    else if((*((V_UINT32 *)(EXTI + PR)) & (1 << 8)) && counter_flag){
         cnt2++;
+        if(cnt2 == 16) cnt2 = 0;
+        *((V_UINT32 *)(EXTI + PR)) |= (1 << 8);
+        delay(100);
+    }
+    else if((*((V_UINT32 *)(EXTI + PR)) & (1 << 7)) && counter_flag){
+        counter_flag = 0;
+        sum = cnt2 + cnt1;
+        if(sum >= 16) sum = sum % 16;
+        *((V_UINT32 *)(EXTI + PR)) |= (1 << 7);
+        delay(100);
     }
 }
