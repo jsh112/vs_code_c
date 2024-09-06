d29f278d620f3807e9542b1841febf06c0a409ce
jsh112 <jshkr1120@gmail.com>
2024-09-06 17:45:03 +0900

    Week_1_C code

--- 24-2/Week1/E.c
+++ 24-2/Week1/E.c
@@ -1,4 +1,4 @@
-// // E : 케이크 자르기2
+// 문제 E : 케이크 자르기2
 // 문제 설명
 // 입력한 2차원 케이크를 2등분하여 출력한다.
 // 근데, 자른 부분은 으깨져서 보이지 않는다.
@@ -28,3 +28,36 @@
 //   1 3 4 1 2
 
 #include <stdio.h>
+
+int main()
+{
+
+    int n;
+    scanf("%d", &n);
+    unsigned int matrix[n][n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%u", &matrix[i][j]);
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if ((i + j) == n - 1)
+            {
+                printf("  ");
+            }
+            else
+            {
+                printf("%u ", matrix[i][j]);
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ 24-2/Week1/I.c
@@ -0,0 +1,55 @@
+// 문제 I : 줄임말
+
+// 문제 설명
+// 공백이 포함된 문장을 입력하면, 대문자만 골라 출력하는 프로그램을 작성하시오.
+// 입력 설명
+// S
+// S(문자열): 처리할 문장
+// (0 <= length(S) < 101)
+
+// 출력 설명
+// 문자열 중 대문자만 고른 줄임말을 출력하라.
+
+// 입력 예시
+// Hi Guys. Nice To Meet You.
+// 출력 예시
+// HGNTMY
+
+// Hint
+// 힌트1:
+// gets(str)
+
+// 위의 함수를 사용하면, 문자열 변수 str에 공백을 포함해서 입력받을 수 있다.
+
+// 힌트2:
+// 문자열의 끝에 NULL 문자가 있어야 %s로 출력할 수 있다.
+
+#include <stdio.h>
+
+int my_strlen(const char *str);
+
+int main()
+{
+
+    char str[101];
+    gets(str);
+    for (int i = 0; i < my_strlen(str); i++)
+    {
+        if ((str[i] >= 'A') && (str[i] <= 'Z'))
+        {
+            printf("%c", str[i]);
+        }
+    }
+
+    return 0;
+}
+
+int my_strlen(const char *str)
+{
+    int cnt = 0;
+    while (str[cnt] != '\0')
+    {
+        cnt++;
+    }
+    return cnt;
+}
