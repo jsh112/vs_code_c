4de7e3ee239a2c726ca04dff9ec3656dc9b53d9e
jsh112 <jshkr1120@gmail.com>
2024-09-23 14:29:47 +0900

    modify

--- baekjoon/Programmers/11.c
+++ baekjoon/Programmers/11.c
@@ -2,45 +2,48 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int power(int x)
-{
-    int n = 1;
-    for (int i = 0; i < x - 1; i++)
-    {
-        n *= 10;
-    }
-    return n;
-}
+// int power(int x)
+// {
+//     int n = 1;
+//     for (int i = 0; i < x - 1; i++)
+//     {
+//         n *= 10;
+//     }
+//     return n;
+// }
 
-// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+// // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+// int solution(const char *n_str)
+// {
+//     int len = 0;
+//     const char *ptr = n_str;
+//     while (*ptr != '\0')
+//     {
+//         len++;
+//         ptr++;
+//     }
+//     int answer = 0, i = power(len);
+//     while (*n_str != '\0')
+//     {
+//         answer += (*n_str - '0') * i;
+//         i /= 10;
+//         n_str++;
+//     }
+
+//     return answer;
+// }
+
 int solution(const char *n_str)
 {
-    int len = 0;
+    int answer = 0;
     const char *ptr = n_str;
     while (*ptr != '\0')
     {
-        len++;
+        answer = 10 * answer + (*ptr - '0');
         ptr++;
     }
-    int answer = 0, i = power(len);
-    while (*n_str != '\0')
-    {
-        answer += (*n_str - '0') * i;
-        i /= 10;
-        n_str++;
-    }
-
     return answer;
 }
-
-// int solution(const char* n_str) {
-//     int answer = 0;
-//     char* ptr = n_str - 1;
-//     while(*++ptr) {
-//         answer = 10 * answer + *ptr - '0';
-//     }
-//     return answer;
-// }
 
 int main()
 {
