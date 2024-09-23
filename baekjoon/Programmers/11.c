f597232a64675c2fdc0bf8089de4d62b701ad53e
jsh112 <jshkr1120@gmail.com>
2024-09-23 14:20:48 +0900

    문자열을 정수로 변환하기

---
+++ baekjoon/Programmers/11.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int power(int x)
+{
+    int n = 1;
+    for (int i = 0; i < x - 1; i++)
+    {
+        n *= 10;
+    }
+    return n;
+}
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+int solution(const char *n_str)
+{
+    int len = 0;
+    const char *ptr = n_str;
+    while (*ptr != '\0')
+    {
+        len++;
+        ptr++;
+    }
+    int answer = 0, d, i = power(len);
+    while (*n_str != '\0')
+    {
+        d = *n_str - '0';
+        answer += d * i;
+        i /= 10;
+        n_str++;
+    }
+
+    return answer;
+}
+
+int main()
+{
+
+    printf("%d\n", solution("8542"));
+
+    return 0;
+}
