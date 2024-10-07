8c6f3e21a9a01eb04422a0b8e242b062e8fbcab0
jsh112 <jshkr1120@gmail.com>
2024-10-07 23:16:52 +0900

    solve the problem

--- 24-2/C/Week6/OJ/D.c
+++ 24-2/C/Week6/OJ/D.c
@@ -6,34 +6,17 @@ int main()
     unsigned int n;
     scanf("%X", &n);
 
-    unsigned int a, b, c, d;
+    unsigned char *it1 = (unsigned char *)&n, tmp;
 
-    a = (n >> 8 * 2) & 0xFF;
-    b = (n >> 8 * 3) & 0xFF;
-    if (b == 0)
-    {
-        printf("%X", a);
-        printf("00");
-    }
-    else if (a != 0 && b != 0)
-    {
-        printf("%X%X", a, b);
-    }
-    a = n & 0xFF;
-    b = (n >> 8 * 1) & 0xFF;
-    if (a == 0)
-    {
-        printf("00%X", b);
-    }
-    else if (b == 0)
-    {
-        printf("%X", a);
-        printf("00");
-    }
-    else
-    {
-        printf("%X%X", a, b);
-    }
+    tmp = *(it1 + 2);
+    *(it1 + 2) = *(it1 + 3);
+    *(it1 + 3) = tmp;
+
+    tmp = *it1;
+    *it1 = *(it1 + 1);
+    *(it1 + 1) = tmp;
+
+    printf("%X", n);
 
     return 0;
 }
